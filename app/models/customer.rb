aca604bac63af4b395efbc5589ac1a8050bf15d2
VuHoangQuanNguyen <hoangquan2561999hl@gmail.com>
2020-03-23 00:52:29 -0500

    add validation + display phone number

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,6 @@
 class Customer < ApplicationRecord
   validates :full_name, presence: true
   has_one_attached :image
+  validates :phone_number, format: { with: /\D*([2-9]\d{2})(\D*)([2-9]\d{2})(\D*)(\d{4})\D*/,
+                                     message: 'Error North America format' }
 end
